142221acfe1a8d811e280637fe5a270c4f3a156e
skippyjonney <jodgarci@gmail.com>
2018-08-22 21:22:47 -0700

    Add instrumentation test

--- app/src/androidTest/java/com/example/jonathan/bakingapp/UI/uiTest.java
+++ app/src/androidTest/java/com/example/jonathan/bakingapp/UI/uiTest.java
@@ -1,21 +1,23 @@
-package com.example.jonathan.bakingapp;
+package com.example.jonathan.bakingapp.UI;
 
 
 import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
-import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import com.example.jonathan.bakingapp.UI.MainActivity;
+import com.example.jonathan.bakingapp.R;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -26,21 +28,20 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.StringStartsWith.startsWith;
 
-
+@LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityScreenTest {
+public class uiTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule =
-            new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
     private IdlingResource mIdlingREsource;
 
     @Before
@@ -50,17 +51,13 @@ public class MainActivityScreenTest {
     }
 
     @Test
-    public void clickRecyclerView_OpensStepActivity(){
-        // Find / Perform / Verify //
-        // Find View and Click
-        onView(withId(R.id.bakingList_rv)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        // Check that activity opens
-        onView(withId(R.id.ingredients_tv)).check(matches(withText(startsWith("Ingredient"))));
-    }
-
-
-    @Test
-    public void fabTest() {
+    public void uiTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.bakingList_rv),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
 
         DataInteraction cardView = onData(anything())
                 .inAdapterView(allOf(withId(R.id.steps_lv),
@@ -68,7 +65,27 @@ public class MainActivityScreenTest {
                                 withClassName(is("android.widget.LinearLayout")),
                                 1)))
                 .atPosition(0);
-        //cardView.perform(click());
+        cardView.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(4997);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.fabNext),
+                        childAtPosition(
+                                allOf(withId(R.id.linearLayout),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                2)),
+                                1),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
 
     }
 
